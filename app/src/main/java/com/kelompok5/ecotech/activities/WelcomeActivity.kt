2a086704b784831d9317545d1a0fa27d7e150a4b
losbanditos <aldimasjaya.07@gmail.com>
2024-06-24 20:44:02 +0700

    feat: edited WelcomeActivity.kt, activity login lupa password register email terkirim

--- app/src/main/java/com/kelompok5/ecotech/activities/WelcomeActivity.kt
+++ app/src/main/java/com/kelompok5/ecotech/activities/WelcomeActivity.kt
@@ -1,5 +1,6 @@
 package com.kelompok5.ecotech.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,9 +11,19 @@ import com.kelompok5.ecotech.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.cvPenyetor.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        binding.cvKolektor.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
