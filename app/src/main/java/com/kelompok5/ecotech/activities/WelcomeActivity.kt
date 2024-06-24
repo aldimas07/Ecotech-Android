01503c50eba771fdd3e52b0edebb8651e2953c3d
losbanditos <aldimasjaya.07@gmail.com>
2024-06-24 14:33:03 +0700

    feat: adding WelcomeActivity.kt and it resources

---
+++ app/src/main/java/com/kelompok5/ecotech/activities/WelcomeActivity.kt
@@ -0,0 +1,18 @@
+package com.kelompok5.ecotech.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kelompok5.ecotech.R
+import com.kelompok5.ecotech.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
