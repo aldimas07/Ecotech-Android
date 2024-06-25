90ded4d8a7df510b0dad9442e857fbc5d9d23df6
losbanditos <aldimasjaya.07@gmail.com>
2024-06-25 16:32:47 +0700

    feat: added feature 2x button back to close app when on selamat datang page

--- app/src/main/java/com/kelompok5/ecotech/activities/WelcomeActivity.kt
+++ app/src/main/java/com/kelompok5/ecotech/activities/WelcomeActivity.kt
@@ -2,11 +2,14 @@ package com.kelompok5.ecotech.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kelompok5.ecotech.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWelcomeBinding
+    private var backPressedTime: Long = 0
+    private lateinit var toast: Toast
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,4 +25,16 @@ class WelcomeActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    override fun onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            toast.cancel()
+            super.onBackPressed()
+            finishAffinity()
+        } else {
+            toast = Toast.makeText(this, "Ketuk sekali lagi untuk keluar dari aplikasi!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+        backPressedTime = System.currentTimeMillis()
+    }
+
 }
