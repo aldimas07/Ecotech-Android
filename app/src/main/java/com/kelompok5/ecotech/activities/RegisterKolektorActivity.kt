79c2bd01585c473a621f57a73d4786d36502143e
losbanditos <aldimasjaya.07@gmail.com>
2024-06-25 14:02:39 +0700

    feat: add new layout activity_register_kolektor.xml and RegisterKolektorActivity.kt

---
+++ app/src/main/java/com/kelompok5/ecotech/activities/RegisterKolektorActivity.kt
@@ -0,0 +1,19 @@
+package com.kelompok5.ecotech.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kelompok5.ecotech.R
+import com.kelompok5.ecotech.databinding.ActivityRegisterKolektorBinding
+
+class RegisterKolektorActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterKolektorBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterKolektorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
